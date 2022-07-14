8c7b3fa87b42e8d280eb34e2bdf3969fb7ecda38
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-07-14 11:59:47 -0500

    configured boot project

---
+++ CFBStatsApp/src/main/java/com/miguelbarrios/cfbapp/CfbStatsAppApplication.java
@@ -0,0 +1,13 @@
+package com.miguelbarrios.cfbapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfbStatsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfbStatsAppApplication.class, args);
+	}
+
+}
