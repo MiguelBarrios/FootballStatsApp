bd796ddc5b641456a8a76c0fa4c16e7b0787172f
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-07-14 00:27:31 -0500

    created player entity

---
+++ JPAFootballApp/src/main/java/com/miguelbarrios/cfbapp/entities/Player.java
@@ -0,0 +1,132 @@
+package com.miguelbarrios.cfbapp.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+/**
+ * Entity implementation class for Entity: Player
+ *
+ */
+@Entity
+
+public class Player {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	
+	private String position;
+	
+	private int height;
+	
+	private int weight;
+	
+	private String year;
+	
+	private int stars;
+	
+	@OneToMany	
+	@JoinColumn(name = "team_id")
+	private Team team;
+	
+
+	public Player() {
+		super();
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getPosition() {
+		return position;
+	}
+
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+
+	public int getHeight() {
+		return height;
+	}
+
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+
+	public int getWeight() {
+		return weight;
+	}
+
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+
+	public String getYear() {
+		return year;
+	}
+
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+
+	public int getStars() {
+		return stars;
+	}
+
+
+	public void setStars(int stars) {
+		this.stars = stars;
+	}
+
+
+	public Team getTeam() {
+		return team;
+	}
+
+
+	public void setTeam(Team team) {
+		this.team = team;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Player [id=" + id + ", name=" + name + ", position=" + position + ", height=" + height + ", weight="
+				+ weight + ", year=" + year + ", stars=" + stars + ", team=" + team + "]";
+	}
+	
+	
+   
+}
---
+++ JPAFootballApp/src/main/java/com/miguelbarrios/cfbapp/entities/Team.java
@@ -0,0 +1,18 @@
+package com.miguelbarrios.cfbapp.entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Team
+ *
+ */
+@Entity
+
+public class Team {
+
+	public Team() {
+		super();
+	}
+   
+}
