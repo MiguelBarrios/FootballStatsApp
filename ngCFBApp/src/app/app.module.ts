938f108a74d7fb502f200a34147e25f35f22c056
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-07-14 22:33:18 -0500

    created player and team services, created matchup component

--- ngCFBApp/src/app/app.module.ts
+++ ngCFBApp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatchupComponent } from './components/matchup/matchup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatchupComponent
   ],
   imports: [
     BrowserModule,
